f1db8c673942b4976fbdbdc5b1e554e45e1b81c2
Shuaib Siddiqui <122801841+sbsidd17@users.noreply.github.com>
2025-05-28 14:36:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Free Storage Server Working'
 
+@app.route('/health')
+def health():
+    return 'OK', 200
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
